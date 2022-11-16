cbe980b0df5974a17724341f8470197e80f007bb
An Sunyoung <nsnyng@naver.com>
2022-11-17 00:04:06 +0900

    style 수정

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,8 +4,8 @@ function ExpenseItem() {
   return (
     <div className="expense-item">
       <div>March 28th 2021</div>
-      <div>
-        <h2 className="expense-item__description">Car Insurance</h2>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
         <div className="expense-item__price">$294.67</div>
       </div>
     </div>
