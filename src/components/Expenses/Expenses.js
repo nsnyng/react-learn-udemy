1a8ca1ac87112601389f9ed1c12d65a42c5478be
An Sunyoung <nsnyng@naver.com>
2022-11-19 22:32:58 +0900

    요소에 key 속성 추가

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,6 +19,7 @@ const Expenses = (props) => {
       />
       {props.items.map((item) => (
         <ExpenseItem
+          key={item.id}
           title={item.title}
           amount={item.amount}
           date={item.date}
